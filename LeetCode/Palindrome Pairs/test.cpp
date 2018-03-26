8498113a1c2c848bb64295b1f1577b8812f53006
tigercosmos <phy.tiger@gmail.com>
2018-03-26 23:39:11 +0800

    other

---
+++ LeetCode/Palindrome Pairs/test.cpp
@@ -0,0 +1,44 @@
+#include "./palindromePairs.cpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+void print(vector<vector<int>> v) {
+  string s = "[";
+  for (int i = 0; i < v.size(); i++) {
+    s += "[";
+    for (int j = 0; j < v[i].size(); j++) {
+      s += to_string(v[i][j]);
+      if (j != v[i].size() - 1)
+        s += " ";
+    }
+    s += "]";
+  }
+  s += "]\n";
+  cout << s << endl;
+}
+
+void assert_equal(vector<vector<int>> v1, vector<vector<int>> v2) {
+  bool isEqual = false;
+  if (v1 == v2) {
+    isEqual = true;
+  }
+  if (!isEqual) {
+    print(v1);
+    print(v2);
+    cout << "the two are not the same!\n";
+    throw "Test failed!";
+  }
+}
+
+int main(int argc, char *argv[]) {
+  Solution sol;
+
+  vector<string> words = {"bat", "tab", "cat"};
+  vector<vector<int>> anwser = {{0, 1}, {1, 0}};
+  assert_equal(sol.palindromePairs(words), anwser);
+
+  words = {"abcd", "dcba", "lls", "s", "sssll"};
+  anwser = {{0, 1}, {1, 0}, {2, 4}, {3, 2}};
+  assert_equal(sol.palindromePairs(words), anwser);
+}
