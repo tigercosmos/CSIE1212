60a1df1daa3e16b45dad060d3be779de8139de47
tigercosmos <phy.tiger@gmail.com>
2018-03-27 15:47:53 +0800

    matrix

--- HW1/matrix.cpp
+++ HW1/matrix.cpp
@@ -7,51 +7,197 @@
 
 Matrix::Matrix(const int &r, const int &c) //constructor
 {
+	row = r;
+	col = c;
+	array = new double *[row];
+	for (int i = 0; i < row; ++i)
+	{
+		array[i] = new double[col];
+		for (int j = 0; j < col; ++j)
+		{
+			array[i][j] = 0;
+		}
+	}
 }
 
-Matrix::Matrix(const Matrix &rhs) //copy constructor
+Matrix::Matrix(const Matrix &m) //copy constructor
 {
+	row = m.row;
+	col = m.col;
+	array = new double *[m.row];
+	for (int r = 0; r < m.row; r++)
+	{
+		array[r] = new double[m.col];
+		for (int c = 0; c < m.col; c++)
+		{
+			array[r][c] = m.array[r][c];
+		}
+	}
 }
 
 Matrix::~Matrix() //destructor
 {
+	for (int i = 0; i < row; ++i)
+	{
+		delete[] array[i];
+	}
+	delete[] array;
 }
 
-double *&Matrix::operator[](const int &idx) const // Enable the use of A[i][j] instead of A.array[i][j]
+double *&Matrix::operator[](const int &idx) const // Enable the use of A[i][j] instead of m.array[i][j]
 {
 	return array[idx];
 }
 
-Matrix Matrix::operator=(const Matrix &rhs) // assignment operator
+Matrix Matrix::operator=(const Matrix &m) // assignment operator
 {
+	row = m.row;
+	col = m.col;
+	array = new double *[m.row];
+	for (int r = 0; r < m.row; r++)
+	{
+		array[r] = new double[m.col];
+		for (int c = 0; c < m.col; c++)
+		{
+			array[r][c] = m.array[r][c];
+		}
+	}
+	return *this;
 }
 
 Matrix Matrix::operator-() const
 {
+	Matrix tmp(*this);
+	for (int r = 0; r < row; r++)
+	{
+		for (int c = 0; c < col; c++)
+		{
+			tmp[r][c] = -array[r][c];
+		}
+	}
+	return tmp;
 }
 
 Matrix Matrix::operator+() const
 {
+	return *this;
 }
 
-Matrix Matrix::operator-(const Matrix &rhs) const
+Matrix Matrix::operator-(const Matrix &m) const
 {
+	Matrix tmp(*this);
+	for (int r = 0; r < row; r++)
+	{
+		for (int c = 0; c < col; c++)
+		{
+			tmp[r][c] = array[r][c] - m[r][c];
+		}
+	}
+	return tmp;
 }
 
-Matrix Matrix::operator+(const Matrix &rhs) const
+Matrix Matrix::operator+(const Matrix &m) const
 {
+	Matrix tmp(*this);
+	for (int r = 0; r < row; r++)
+	{
+		for (int c = 0; c < col; c++)
+		{
+			tmp[r][c] = array[r][c] + m[r][c];
+		}
+	}
+	return tmp;
 }
 
-Matrix Matrix::operator*(const Matrix &rhs) const
+Matrix Matrix::operator*(const Matrix &m) const
 {
+	Matrix temp(row, m.col);
+	for (int i = 0; i < temp.row; i++)
+	{
+		for (int j = 0; j < temp.col; j++)
+		{
+			for (int k = 0; k < col; k++)
+			{
+				temp.array[i][j] += (array[i][k] * m.array[k][j]);
+			}
+		}
+	}
+	return temp;
 }
 
-Matrix Matrix::operator/(const Matrix &rhs) const
+Matrix Matrix::operator/(const Matrix &m) const
 {
+	return *this * m.inverse();
 }
 
 Matrix Matrix::inverse() const
 {
+	// Gauss-Jordan Elimination
+	if (row != col)
+	{
+		throw "Cannot get inverse if not N x N!";
+	}
+	int matrixSize = row;
+	// Step 1. Make [A] to [A|I]
+	Matrix temp(matrixSize, 2 * matrixSize);
+	for (int r = 0; r < matrixSize; r++)
+	{
+		for (int c = 0; c < matrixSize; c++)
+		{
+			temp[r][c] = array[r][c];
+		}
+		temp[r][matrixSize + r] = 1;
+	}
+
+	// Step 2. Create upper triangular matrix
+	for (int i = 0; i < matrixSize - 1; i++)
+	{
+		for (int r = i + 1; r < matrixSize; r++)
+		{
+			if (temp[r][i] != 0)
+			{
+				double coefficient = temp[i][i] / temp[r][i];
+				for (int c = 0; c < matrixSize * 2; c++)
+				{
+					temp[r][c] = temp[i][c] - coefficient * temp[r][c];
+				}
+			}
+		}
+	}
+
+	// Normolize left part
+	for (int r = 0; r < matrixSize; r++)
+	{
+		double coefficient = temp[r][r];
+		for (int c = 0; c < matrixSize * 2; c++)
+		{
+			temp[r][c] /= coefficient;
+		}
+	}
+
+	// Step 3. From upper triangular matrix get [I|B]
+	for (int i = matrixSize - 1; i > 0; i--)
+	{
+		for (int r = i - 1; r >= 0; r--)
+		{
+			double coefficient = -temp[r][i];
+			for (int c = 0; c < matrixSize * 2; c++)
+			{
+				temp[r][c] += coefficient * temp[i][c];
+			}
+		}
+	}
+
+	// Step 4. Get B
+	Matrix result(matrixSize, matrixSize);
+	for (int r = 0; r < matrixSize; r++)
+	{
+		for (int c = 0; c < matrixSize; c++)
+		{
+			result[r][c] = temp[r][matrixSize + c];
+		}
+	}
+	return result;
 }
 
 void Matrix::read(const char *fn)
