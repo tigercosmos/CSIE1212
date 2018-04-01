09aa608ebb98ff90fb93641b4bdda77b6e4e9e1b
tigercosmos <phy.tiger@gmail.com>
2018-04-02 01:25:42 +0800

    change to structure

--- Course/HW2/main.cpp
+++ Course/HW2/main.cpp
@@ -8,21 +8,7 @@
 #include <vector>
 using namespace std;
 
-class Data {
-public:
-  void setDate(string i) { date = i; }
-  void setCurrency(string s) { currency = s; }
-  void setExchange(string s) { exchange = s; }
-  void setLow(float f) { low = f; }
-  void setHigh(float f) { high = f; }
-  void setCapital(long l) { capital = l; }
-  string getDate() { return date; }
-  string getCurrency() { return currency; }
-  string getExchange() { return exchange; }
-  float getLow() { return low; }
-  float getHigh() { return high; }
-  long getCapital() { return capital; }
-
+struct Data {
   // void print();
   // void printInLine();
 
@@ -41,22 +27,22 @@ static void split(Data &d, const string &s, char delimiter) {
   while (getline(tokenStream, token, delimiter)) {
     switch (i) {
     case 0:
-      d.setDate(token);
+      d.date = token;
       break;
     case 1:
-      d.setCurrency(token);
+      d.currency = token;
       break;
     case 2:
-      d.setExchange(token);
+      d.exchange = token;
       break;
     case 3:
-      d.setLow(stof(token));
+      d.low = stof(token);
       break;
     case 4:
-      d.setHigh(stof(token));
+      d.high = stof(token);
       break;
     case 5:
-      d.setCapital(stol(token));
+      d.capital = stol(token);
       break;
     default:
       break;
@@ -143,9 +129,9 @@ int main(int argc, char *argv[]) {
     }
 
     if (tokens[0] == "query") {
-      tmp->setDate(tokens[1]);
-      tmp->setCurrency(tokens[2]);
-      tmp->setExchange(tokens[3]);
+      tmp->date = tokens[1];
+      tmp->currency = tokens[2];
+      tmp->exchange = tokens[3];
       vector<Data>::iterator date_low, date_up;
       vector<Data>::iterator currency_low;
       vector<Data>::iterator exchange_low, exchange_up;
@@ -154,11 +140,11 @@ int main(int argc, char *argv[]) {
                                  exchangeCmp);
       exchange_up = upper_bound(datum_query.begin(), datum_query.end(), *tmp,
                                 exchangeCmp);
-      if (exchange_low->getExchange() != tmp->getExchange()) {
+      if (exchange_low->exchange != tmp->exchange) {
         cout << "none" << endl;
         continue;
       }
-      if (exchange_low->getExchange() != tmp->getExchange()) {
+      if (exchange_low->exchange != tmp->exchange) {
         exchange_up -= 1;
       }
 
@@ -166,32 +152,32 @@ int main(int argc, char *argv[]) {
       date_low = lower_bound(exchange_low, exchange_up, *tmp, dateCmp);
       date_up = upper_bound(exchange_low, exchange_up, *tmp, dateCmp);
       // if not find
-      if (date_low->getDate() != tmp->getDate()) {
+      if (date_low->date != tmp->date) {
         cout << "none" << endl;
         continue;
       }
       // check the upper bound
-      if (date_up->getDate() != tmp->getDate()) {
+      if (date_up->date != tmp->date) {
         date_up -= 1;
       }
 
       currency_low = lower_bound(date_low, date_up, *tmp, currencyCmp);
-      if (currency_low->getCurrency() != tmp->getCurrency()) {
+      if (currency_low->currency != tmp->currency) {
         cout << "none" << endl;
         continue;
       }
 
       int index = distance(datum_query.begin(), currency_low);
-      cout << fixed << setprecision(4) << datum_query[index].getLow() << " "
-           << datum_query[index].getHigh() << " "
-           << datum_query[index].getCapital() << endl;
+      cout << fixed << setprecision(4) << datum_query[index].low << " "
+           << datum_query[index].high << " " << datum_query[index].capital
+           << endl;
     } else if (tokens[0] == "price") {
       vector<Data>::iterator date_low, date_up;
       vector<Data>::iterator currency_low, currency_up;
       vector<Data>::iterator m_low, m_high; // min or max
       // arguments
-      tmp->setDate(tokens[2]);
-      tmp->setCurrency(tokens[3]);
+      tmp->date = tokens[2];
+      tmp->currency = tokens[3];
 
       currency_low = lower_bound(date_low, date_up, *tmp, currencyCmp);
       currency_up = upper_bound(date_low, date_up, *tmp, currencyCmp);
@@ -202,38 +188,38 @@ int main(int argc, char *argv[]) {
       currency_up =
           upper_bound(datum_m.begin(), datum_m.end(), *tmp, currencyCmp);
 
-      if (currency_low->getCurrency() != tmp->getCurrency()) {
+      if (currency_low->currency != tmp->currency) {
         cout << "none" << endl;
         continue;
       }
-      if (currency_up->getCurrency() != tmp->getCurrency()) {
+      if (currency_up->currency != tmp->currency) {
         currency_up -= 1;
       }
 
       date_low = lower_bound(currency_low, currency_up, *tmp, dateCmp);
       date_up = upper_bound(currency_low, currency_up, *tmp, dateCmp);
       // if not find
-      if (date_low->getDate() != tmp->getDate()) {
+      if (date_low->date != tmp->date) {
         cout << "none" << endl;
         continue;
       }
       // check the upper bound
-      if (date_up->getDate() != tmp->getDate()) {
+      if (date_up->date != tmp->date) {
         date_up -= 1;
       }
 
       if (tokens[1] == "min") {
         sort(date_low, date_up + 1, lowCmp);
-        cout << fixed << setprecision(4) << date_low->getLow() << endl;
+        cout << fixed << setprecision(4) << date_low->low << endl;
       } else {
         sort(date_low, date_up + 1, highCmp);
-        cout << fixed << setprecision(4) << date_up->getHigh() << endl;
+        cout << fixed << setprecision(4) << date_up->high << endl;
       }
     } else if (tokens[0] == "cap") {
       vector<Data>::iterator date_low, date_up;
       vector<Data>::iterator exchange_low, exchange_up;
-      tmp->setDate(tokens[1]);
-      tmp->setExchange(tokens[2]);
+      tmp->date = tokens[1];
+      tmp->exchange = tokens[2];
 
       // binary search lower and upper
       exchange_low = lower_bound(datum_query.begin(), datum_query.end(), *tmp,
@@ -241,12 +227,12 @@ int main(int argc, char *argv[]) {
       exchange_up = upper_bound(datum_query.begin(), datum_query.end(), *tmp,
                                 exchangeCmp);
       // if not find
-      if (exchange_low->getExchange() != tmp->getExchange()) {
+      if (exchange_low->exchange != tmp->exchange) {
         cout << "none" << endl;
         continue;
       }
       // check the upper bound
-      if (exchange_low->getExchange() != tmp->getExchange()) {
+      if (exchange_low->exchange != tmp->exchange) {
         exchange_up -= 1;
       }
 
@@ -254,18 +240,18 @@ int main(int argc, char *argv[]) {
       date_low = lower_bound(exchange_low, exchange_up, *tmp, dateCmp);
       date_up = upper_bound(exchange_low, exchange_up, *tmp, dateCmp);
       // if not find
-      if (date_low->getDate() != tmp->getDate()) {
+      if (date_low->date != tmp->date) {
         cout << "none" << endl;
         continue;
       }
       // check the upper bound
-      if (date_up->getDate() != tmp->getDate()) {
+      if (date_up->date != tmp->date) {
         date_up -= 1;
       }
 
       long sum = 0;
       for (vector<Data>::iterator it = date_low; it <= date_up; it++) {
-        sum += it->getCapital();
+        sum += it->capital;
       }
       cout << sum << endl;
     } else {
