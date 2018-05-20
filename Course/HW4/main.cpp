29ccadc843ae13d49e6fb732be427b553da73bde
tigercosmos <phy.tiger@gmail.com>
2018-05-21 02:24:35 +0800

    hash + map

--- Course/HW4/main.cpp
+++ Course/HW4/main.cpp
@@ -3,29 +3,47 @@
 */
 #include <algorithm>
 #include <bitset>
+#include <cmath>
+#include <deque>
 #include <functional>
 #include <iostream>
 #include <map>
 #include <new>
-#include <cmath>
 #include <string>
 #include <unordered_map>
 #include <vector>
 using namespace std;
 
+// #define debug
+
 const unsigned PRIME_BASE = 29;
 const unsigned PRIME_MOD = 1000000007;
 
-long long make_hash(const string &s) {
+map<long long, int> hash_map;
+
+char origin_string[200000];
+int front = 100000, back = 100000 - 1;
+string target_string;
+
+#ifdef debug
+void print() {
+  for (int i = front; i <= back; i++) {
+    cout << origin_string[i];
+  }
+  cout << '\n';
+}
+#endif
+
+inline long long make_hash(const string &s) {
   long long ret = 0;
-  for (int i = 0; i < s.size(); i++) {
+  for (int i = 0; i < s.length(); i++) {
     ret = ret * PRIME_BASE + s[i];
     ret %= PRIME_MOD;
   }
   return ret;
 }
 
-long long get_power(const int len) {
+inline long long get_power(const int len) {
   int exp = -1;
   int tmp = 1;
   while (tmp < len) {
@@ -33,66 +51,215 @@ long long get_power(const int len) {
     ++exp;
   }
   long long &&power = PRIME_BASE;
-  for (int i = 0; i < exp; ++i) {
-    power = (power * power) % PRIME_MOD;
-  }
-  for (int i = pow(2,exp); i < len; ++i) {
-    power = (power * PRIME_BASE) % PRIME_MOD;
+  if (len > 1) {
+    for (int i = 0; i < exp; ++i) {
+      power = (power * power) % PRIME_MOD;
+    }
+    for (int i = pow(2, exp); i < len; ++i) {
+      power = (power * PRIME_BASE) % PRIME_MOD;
+    }
   }
   return move(power);
 }
 
-unsigned roll_hash(const string &sub_string, const string &main_string) {
-  unsigned&& counter = 0;
-  long long sub_hash = make_hash(sub_string);
+int compare() {
+  int count = 0;
+#ifdef debug
+  cout << front << " " << back << endl;
+#endif
+  for (int i = front; i <= back - target_string.length() + 1; ++i) {
+    int flag = 1;
+#ifdef debug
+    print();
+    for (int j = front; j < i; ++j) {
+      cout << " ";
+    }
+    for (int j = 0; j < target_string.length(); ++j) {
+      cout << origin_string[i + j];
+    }
+    cout << endl;
+    for (int j = front; j < i; ++j) {
+      cout << " ";
+    }
+    for (int j = 0; j < target_string.length(); ++j) {
+      cout << target_string[j];
+    }
+    cout << endl;
+#endif
+    for (int j = 0; j < target_string.length(); ++j) {
+      if (origin_string[i + j] != target_string[j]) {
+        flag = 0;
+        break;
+      }
+    }
+    if (flag) {
+#ifdef debug
+      for (int j = front; j < i; ++j) {
+        cout << " ";
+      }
+      cout << "^" << endl;
+#endif
+      ++count;
+    }
+  }
+  return count;
+}
+
+inline unsigned roll_hash() {
+  unsigned &&counter = 0;
+  long long sub_hash = make_hash(target_string);
   long long main_hash = 0;
-  int sub_string_size = sub_string.size();
+  int sub_string_size = target_string.length();
 
   long long power = get_power(sub_string_size);
 
-  for (int i = 0; i < main_string.size(); i++) {
+  for (int i = front; i <= back; ++i) {
+
     // add the last letter
-    main_hash = main_hash * PRIME_BASE + main_string[i];
+    main_hash = main_hash * PRIME_BASE + origin_string[i];
 
     main_hash %= PRIME_MOD;
 
     // remove the first character, if needed
-    if (i >= sub_string_size) {
-      main_hash -= power * main_string[i - sub_string_size] % PRIME_MOD;
+    if ((i - front) >= sub_string_size) {
+      main_hash -= (power * origin_string[i - sub_string_size] % PRIME_MOD);
+
       if (main_hash < 0) // negative can be made positive with mod
         main_hash += PRIME_MOD;
     }
-
+#ifdef debug
+    print();
+    for (int j = front; j < i; ++j) {
+      cout << " ";
+    }
+    cout << "^\n";
+    cout << main_hash << endl;
+    cout << sub_hash << endl;
+#endif
     // match?
-    if (i >= sub_string_size - 1 && main_hash == sub_hash)
+    if ((i - front) >= sub_string_size - 1 && main_hash == sub_hash) {
+#ifdef debug
+      cout << "\033[1;36m====MATCH====\033[0m" << endl;
+#endif
       ++counter;
+    }
+#ifdef debug
+    cout << "-----\n";
+#endif
   }
 
   return move(counter);
 }
 
+inline void roll_hash_map() {
+  for (int len = 1; len <= 10 && len <= (back - front + 1); ++len) {
+    long long main_hash = 0;
+
+    long long power = get_power(len);
+
+    for (int i = front; i <= back; ++i) {
+
+      // add the last letter
+      main_hash = main_hash * PRIME_BASE + origin_string[i];
+
+      main_hash %= PRIME_MOD;
+
+      // remove the first character, if needed
+      if ((i - front) >= len) {
+        main_hash -= (power * origin_string[i - len] % PRIME_MOD);
+
+        if (main_hash < 0) // negative can be made positive with mod
+          main_hash += PRIME_MOD;
+      }
+
+      if ((i - front) >= len - 1) {
+        auto search = hash_map.find(main_hash);
+        if (search != hash_map.end()) {
+          hash_map[main_hash] += 1;
+        } else {
+          hash_map.insert({main_hash, 1});
+        }
+      }
+    }
+  }
+}
+
+inline void roll_hash_map_front() {
+  for (int len = 1; len <= 10 && len <= (back - front + 1); ++len) {
+    long long main_hash = 0;
+
+    for (int i = front; i <= front + len - 1; ++i) {
+      // add the last letter
+      main_hash = main_hash * PRIME_BASE + origin_string[i];
+
+      main_hash %= PRIME_MOD;
+    }
+    auto search = hash_map.find(main_hash);
+    if (search != hash_map.end()) {
+      hash_map[main_hash] += 1;
+    } else {
+      hash_map.insert({main_hash, 1});
+    }
+  }
+}
+
+inline void roll_hash_map_back() {
+  for (int len = 1; len <= 10 && len <= (back - front + 1); ++len) {
+    long long main_hash = 0;
+
+    for (int i = back - len + 1; i <= back; ++i) {
+      // add the last letter
+      main_hash = main_hash * PRIME_BASE + origin_string[i];
+
+      main_hash %= PRIME_MOD;
+    }
+    auto search = hash_map.find(main_hash);
+    if (search != hash_map.end()) {
+      hash_map[main_hash] += 1;
+    } else {
+      hash_map.insert({main_hash, 1});
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
-  string origin_string;
+
   int steps;
   char command;
-  string prefix_string;
-  string suffix_string;
-  string target_string;
-  cin >> origin_string >> steps;
+  char tmp_char;
+  char suffix_char;
+  target_string.reserve(100000);
+
+  cin >> target_string >> steps;
+
+  for (int i = 0; i < target_string.length(); ++i) {
+    origin_string[++back] = target_string[i];
+  }
+  roll_hash_map();
   for (int i = 0; i < steps; ++i) {
-    cin >> command >> target_string;
+    cin >> command;
     if (command == '1') {
-      prefix_string.insert(0, target_string);
+      cin >> tmp_char;
+      origin_string[--front] = tmp_char;
+      roll_hash_map_front();
     } else if (command == '2') {
-      suffix_string += target_string;
+      cin >> tmp_char;
+      origin_string[++back] = tmp_char;
+      roll_hash_map_back();
     } else {
-      origin_string.insert(0, prefix_string);
-      origin_string += suffix_string;
-      cout << roll_hash(target_string, origin_string) << "\n";
-      prefix_string = "";
-      suffix_string = "";
+      cin >> target_string;
+      if (target_string.length() > 10) {
+        cout << roll_hash() << "\n";
+      } else {
+        auto search = hash_map.find(make_hash(target_string));
+        if (search != hash_map.end()) {
+          cout << search->second << '\n';
+        } else {
+          cout << "0\n";
+        }
+      }
     }
   }
 }
