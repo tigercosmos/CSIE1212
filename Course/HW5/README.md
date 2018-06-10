711547cedcb6b69cea4f99be018d1ae2a50b94df
tigercosmos <phy.tiger@gmail.com>
2018-06-09 21:00:11 -0700

    init

---
+++ Course/HW5/README.md
@@ -0,0 +1,3 @@
+## HW 5-1：Peipei and Frenchfries
+
+[Question link](https://tioj.ck.tp.edu.tw/pmisc/ntudsa/hw5-1.html)
---
+++ Course/HW5/main.cpp
@@ -0,0 +1,16 @@
+/*
+** Copyright @ Liu, An-Chi
+*/
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+// #define debug
+
+int main(int argc, char *argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+}
