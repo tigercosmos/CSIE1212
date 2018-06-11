9a6bf91f0d78bf5366e1cccc296067417573c94e
tigercosmos <phy.tiger@gmail.com>
2018-06-11 20:25:50 +0800

    init

---
+++ Course/HW6/README.md
@@ -0,0 +1,3 @@
+## HW 5-2：Big Brother and Angry Birds
+
+[Question link](https://tioj.ck.tp.edu.tw/pmisc/ntudsa/hw5-2.html)
---
+++ Course/HW6/main.cpp
@@ -0,0 +1,17 @@
+/*
+** Copyright @ Liu, An-Chi
+*/
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <new>
+#include <vector>
+using namespace std;
+
+#define debug
+
+int main(int argc, char *argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+}
