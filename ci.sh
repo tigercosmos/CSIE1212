00ea11441e3d922d7421f542378e95a9b21b11a3
tigercosmos <phy.tiger@gmail.com>
2018-06-12 15:39:06 +0800

    ci

--- ci.sh
+++ ci.sh
@@ -7,12 +7,10 @@ for i in Leetcode/* ; do
     cd $ROOT
   fi
 done
-cd Course/HW1
-echo "Testing make HW1 ...."
-make
-cd "$ROOT/Course/HW2"
-echo "Testing make HW2 ...."
-time -f "%Us %MkB" make test
-cd "$ROOT/Course/HW3"
-echo "Testing make HW3 ...."
-make test1
+for i in Course/* ; do
+  if [ -d "$i" ]; then
+    echo "Testing make $i ...."
+    cd "$i" && make
+    cd $ROOT
+  fi
+done
